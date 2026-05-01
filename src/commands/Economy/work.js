cc9ec6e9a92c0a56f8b59dbe6651aec3bd383588
VessTime777 <vesstime@mail.ru>
2026-05-01 22:06:07 +0600

    Update work.js

--- src/commands/Economy/work.js
+++ src/commands/Economy/work.js
@@ -6,8 +6,8 @@ import { logger } from '../../utils/logger.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 
 const WORK_COOLDOWN = 30 * 60 * 1000;
-const MIN_WORK_AMOUNT = 500000;
-const MAX_WORK_AMOUNT = 300000000;
+const MIN_WORK_AMOUNT = 30;
+const MAX_WORK_AMOUNT = 300;
 const LAPTOP_MULTIPLIER = 1.5;
 const WORK_JOBS = [
     "Software Developer",

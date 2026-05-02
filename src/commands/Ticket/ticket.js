d0e40a7727c5a65b94645f7f89d5b5c998da1775
VessTime777 <vesstime@mail.ru>
2026-05-02 16:09:02 +0600

    Update ticket.js

--- src/commands/Ticket/ticket.js
+++ src/commands/Ticket/ticket.js
@@ -163,7 +163,7 @@ description: panelMessage,
                     .setCustomId("create_ticket")
 .setLabel(buttonLabel)
                     .setStyle(ButtonStyle.Primary)
-                    .setEmoji(""),
+                    .setEmoji("📩"),
             );
 
             try {
@@ -331,6 +331,3 @@ description: panelMessage,
         }
     }
 };
-
-
-
